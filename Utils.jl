16b17c7337600d4fa37b07ef5b9b7ee7d3247c8b
Jeremy Boulet <jeremyboulet85@gmail.com>
2019-12-18 15:13:36 -0500

    initial commit

--- Utils.jl
+++ Utils.jl
@@ -8,3 +8,24 @@ function partitionTrainTest(data, at = 0.7)
     test_idx = view(idx, (floor(Int, at*n)+1):n)
     data[train_idx,:], data[test_idx,:]
 end
+
+
+# Function pour remplacer les missing avec la moyenne de la ligne
+# data: dataframe
+# nbCol: nombre de colonnes pour la moyenne
+# offset: nombre de colonnes a skip avant les donnees
+function rowMean(data, nbCol, offset)
+    for i = 1:size(data, 1)
+        sum = 0
+        for k = 1:nbCol
+            if data[i,j + offset] !== missing
+                sum = sum + data[i,j + offset]
+            end
+        end
+        for k = 1:nbCol
+            if data[i,j + offset] === missing
+                data[i,j + offset] = floor(sum/5)
+            end
+        end
+    end
+end
